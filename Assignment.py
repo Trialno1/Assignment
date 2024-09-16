aac6dc798f89cccf88c794eb717df9804f4627f0
Trialno1 <50708982+Trialno1@users.noreply.github.com>
2024-09-16 22:18:21 +0300

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Examples
+
+# This notebook provides an overview of various data science tools and languages. It includes examples of arithmetic operations and data conversions.
+# 
+
+# ## Data Science Languages
+# - Python
+# - R
+# - SQL
+# - Scala
+# 
+
+# ## Data Science Libraries
+# - Pandas
+# - NumPy
+# - Scikit-learn
+# - TensorFlow
+# 
+
+# ## Data Science Tools
+# | Tool          | Description                              |
+# |---------------|------------------------------------------|
+# | Jupyter       | Interactive computing environment        |
+# | GitHub         | Version control and collaboration platform|
+# | Apache Spark  | Big data processing framework            |
+# | Tableau       | Data visualization software               |
+# 
+
+# ## Arithmetic Expression Examples
+# In this section, we will demonstrate basic arithmetic operations such as addition and multiplication.
+# 
+
+# In[2]:
+
+
+# Multiplying and Adding Numbers
+a = 5
+b = 10
+multiplication = a * b
+addition = a + b
+multiplication, addition
+
+
+# In[3]:
+
+
+# Convert Minutes to Hours
+minutes = 120
+hours = minutes / 60
+hours
+
+
+# ## Objectives
+# - Understand data science languages and libraries
+# - Learn basic arithmetic operations
+# - Convert time units
+# 
+
+# ## Author
+# Mai
+# 
+
+# In[ ]:
+
+
+
+
